f7206e22b77c59e0d506fad5d08fcd87c047d297
Stoicism-Sympatheia <30344545+stoicism-sympathei@users.noreply.github.com>
2023-05-06 14:21:51 +0300

    States and Cities

--- web_dynamic/100-hbnb.py
+++ web_dynamic/100-hbnb.py
@@ -2,9 +2,10 @@
 """
 Flask App that integrates with AirBnB static HTML Template
 """
-import uuid
 from flask import Flask, render_template, url_for
 from models import storage
+from uuid import uuid4, UUID
+
 
 # flask setup
 app = Flask(__name__)
@@ -28,19 +29,18 @@ def hbnb_filters(the_id=None):
     """
     handles request to custom template with states, cities & amentities
     """
-    states = storage.all('State').values()
+    state_objs = storage.all('State').values()
+    states = dict([state.name, state] for state in state_objs)
     amens = storage.all('Amenity').values()
     places = storage.all('Place').values()
     users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
                  for user in storage.all('User').values())
-    cache_id = (str(uuid.uuid4()))
     return render_template('100-hbnb.html',
                            states=states,
                            amens=amens,
                            places=places,
                            users=users,
-                           cache_id=cache_id)
-
+                           cache_id=str(uuid4()))
 
 if __name__ == "__main__":
     """
